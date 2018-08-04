35cf958d582b28a798cf9b1924d1e8f3f8e14772
viktharien <viktharien@zoho.com>
2018-08-04 11:27:28 -0400

    s

---
+++ lib/gdec/version.rb
@@ -0,0 +1,3 @@
+module Gdec
+  VERSION = "0.1.0"
+end
---
+++ spec/gdec_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Gdec do
+  it "has a version number" do
+    expect(Gdec::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
